41c8181445c2d890c33f4918bb1ac59c596c35c7
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-08-16 12:26:19 -0300

    build: implement pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWA from "next-pwa";
+
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+};
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPWA(pwaConfig)(nextConfig);
