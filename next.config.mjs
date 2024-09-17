7859e6850b54d3839b99ca7de0770820862c3e87
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-09-17 15:11:30 -0300

    feat: configure Next.js with PWA support and styled-components
    This commit adds PWA (Progressive Web App) support to the Next.js configuration. It registers the service worker, enables skipping waiting, and disables PWA in development mode. Additionally, it enables the usage of styled-components in the compiler configuration.
    
    #1

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,19 @@
 
 import withPWA from "next-pwa";
 
-const pwaConfig = {
+const nextPWA = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-};
+});
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
-export default withPWA(pwaConfig)(nextConfig);
+export default nextPWA(nextConfig);
