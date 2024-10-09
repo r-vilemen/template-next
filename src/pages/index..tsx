3d61d9a6e8a24bb9ec099bba906f274f8646e56f
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-09 07:04:22 -0300

    feat: Atualizar página inicial com título "home page"

--- src/pages/index..tsx
+++ src/pages/index..tsx
@@ -1,11 +1,9 @@
-import { Header } from "@/components/Header";
-import Image from "next/image";
-import HomePage from "./home";
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-      <HomePage />
+      <h1>home page</h1>
     </div>
   );
 }
