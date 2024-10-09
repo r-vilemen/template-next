d8c153c6a5f4f87d9b5ddaf31148cff40483849d
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-09 07:29:20 -0300

    feat: Adicionar página 404 e estilos
    Adiciona a página 404 com mensagem de erro e botão para retornar à página inicial. Também inclui os estilos para a página 404.

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Container } from "@/ui/styles/pages/404/styles";
+
+export default function Custom404() {
+  const router = useRouter();
+
+  return (
+    <Container>
+      <div>
+        <div>
+          <span onClick={() => router.push("/")} />
+        </div>
+
+        <div>
+          <h1>404</h1>
+          <p>Esta página não foi encontrada</p>
+          <button onClick={() => router.push("/")}>
+            Ir para página inicial
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+}
