17a0af5b378bbc260536fb6b2925dd031fe0cb8e
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-09 07:03:18 -0300

    feat: Adicionar componente Layout
    Adiciona o componente Layout que inclui o cabeçalho, o conteúdo principal e o rodapé da página.

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
