d6b1614c55bcc0aaabc937f1867de667621e5c03
Rodrigo Vilemen <67611716+r-vilemen@users.noreply.github.com>
2024-10-10 07:40:25 -0300

    fix: break tablet in theme

---
+++ src/Styles/theme.ts
@@ -0,0 +1,29 @@
+export default {
+  breaks: {
+    minimobile: "max-width: 340px",
+    mobile: "max-width: 767px",
+    tablet: "(min-width: 768px) and (max-width: 1024px)",
+    desktop: "min-width: 1024px",
+    xs: "min-width: 0px",
+    sm: "min-width: 576px",
+    md: "min-width: 768px",
+    lg: "min-width: 992px",
+    xl: "min-width: 1200px",
+    xxl: "min-width: 1400px",
+  },
+};
+
+// export default {
+//   breaks: {
+//     minimobile: "(max-width: 340px)",
+//     mobile: "(max-width: 767px)",
+//     tablet: "(min-width: 768px) and (max-width: 1024px)",
+//     desktop: "(min-width: 1024px)",
+//     xs: "(min-width: 0px)",
+//     sm: "(min-width: 576px)",
+//     md: "(min-width: 768px)",
+//     lg: "(min-width: 992px)",
+//     xl: "(min-width: 1200px)",
+//     xxl: "(min-width: 1400px)",
+//   },
+// };
